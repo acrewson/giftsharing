fd7c72d1826c865e14c62ce82f5db8ffcb5a8454
acrewson <acrewson@gmail.com>
2014-03-05 10:36:16 -0600

    update 3/5 - connections page

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -6,6 +6,8 @@ def home
   # Find people I am connected to
   @my_connections = User.select("users.*, connections.connection_type_id").joins("JOIN connections ON connections.connected_user_id = users.id AND connections.user_id = #{@current_user.id}")
 
+  @my_pending_connections = User.joins(:connection_requests).where("requested_user_id = ?", @current_user.id)
+
 end
 
 end
