05bd974ab1f9c54acaa3eae68a960e886172baa5
acrewson <acrewson@gmail.com>
2014-09-13 17:03:08 -0500

    Updates 9/13

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,8 @@ class UserMailer < ActionMailer::Base
  def account_create_verify_email(temp_user)
     @temp_user = temp_user
     # @url = "http://localhost:3000/?verify=#{@temp_user.security_code}"
-    @url  = "http://afternoon-inlet-6717.herokuapp.com/?verify=#{@temp_user.security_code}"
+    # @url  = "http://afternoon-inlet-6717.herokuapp.com/?verify=#{@temp_user.security_code}"
+    @url  = "http://www.savvygifter.com/?verify=#{@temp_user.security_code}"
     mail(to: @temp_user.email, subject: "Welcome to Gift Sharing, #{@temp_user.firstname}!")
   end
 
@@ -16,14 +17,16 @@ class UserMailer < ActionMailer::Base
     @current_user = current_user
     @requested_user = requested_user
     # @url  = 'http://localhost:3000/'
-    @url  = 'http://afternoon-inlet-6717.herokuapp.com'
+    # @url  = 'http://afternoon-inlet-6717.herokuapp.com'
+    @url  = 'http://www.savvygifter.com'
     mail(to: @requested_user.email, subject: "#{@current_user.firstname} would like to connect on Gift Share!")
   end
 
  def connection_invite_email(current_user, requested_email)
     @current_user = current_user
     @requested_email = requested_email
-    @url  = 'http://afternoon-inlet-6717.herokuapp.com'
+    # @url  = 'http://afternoon-inlet-6717.herokuapp.com'
+    @url  = 'http://www.savvygifter.com'
     mail(to: @requested_email, subject: "#{@current_user.firstname} would like to connect on Gift Share!")
   end
 
