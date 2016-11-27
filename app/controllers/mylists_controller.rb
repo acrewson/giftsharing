760b184530393123401331e6adedf5e110bfdd3b
acrewson <acrewson@gmail.com>
2016-11-26 23:02:37 -0500

    fix link logic

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -150,7 +150,7 @@ class MylistsController < ApplicationController
     i.comments = params[:comments]
     i.url = params[:url]
 
-    if params[:url][0,7] != "http://" & params[:url][0,8] != "https://"
+    if params[:url][0,7] != "http://" and params[:url][0,8] != "https://"
       i.url = "http://" + i.url
     end
 
